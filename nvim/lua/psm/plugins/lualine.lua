2a8cc4185d02a516a98d32849fe2260a6192d859
argus <tjdals4047@gmail.com>
2026-09-12 20:16:07 +0900

    feat(nvim): show absolute file paths in lualine

--- nvim/lua/psm/plugins/lualine.lua
+++ nvim/lua/psm/plugins/lualine.lua
@@ -66,7 +66,7 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = { { "filename", path = 2 } },
 				lualine_x = {
 					{
 						lazy_status.updates,
